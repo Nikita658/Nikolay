ceeb0334f285812af72cf5d1e4509a120e3c2ed8
Nikita658 <nikita14bykov@gmail.com>
2021-08-04 21:14:55 +0300

    i doing Homerwork

---
+++ src/Task1.java
@@ -0,0 +1,20 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        System.out.println(proverkaNaChetnost(17));
+square();
+    }
+
+    private static boolean proverkaNaChetnost(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private static void square(){
+
+    }
+}
